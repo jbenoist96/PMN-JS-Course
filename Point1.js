50ff2bc3337090c06563bd000e4f354bc95fbec5
jbenoist96 <jbenoist@la-pmn.org>
2021-02-23 11:43:29 +0100

    add the first vesrion of the Point exercise 1

---
+++ Point1.js
@@ -0,0 +1,46 @@
+class Point{
+
+    //Constr
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //getters
+    getX(){
+        return this.x;
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    //setters
+    setX(x){
+        this.x = x;
+    }
+
+    setY(y){
+        this.y = y;
+    }
+
+    //toString
+    toString(){
+        return `votre point se situe en (${this.x}, ${this.y})`;
+    }
+
+}
+
+//Main
+
+//Init
+let p1 = new Point(2,1);
+
+//Test
+console.log(p1.getX());
+console.log(p1.getY());
+console.log(p1.toString());
+
+p1.setX(3);
+p1.setY(0);
+console.log(p1.toString());
