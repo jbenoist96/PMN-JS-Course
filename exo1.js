55352a71ce6c417f5a6178182bd5d089b9e1f816
jbenoist96 <jbenoist@la-pmn.org>
2021-02-23 09:26:49 +0100

    Add the final version of the first exercise

--- exo1.js
+++ exo1.js
@@ -1,1 +1,16 @@
-console.log(`aaaa`);
+//Lib
+var scanf = require("scanf");
+
+//Input 
+console.log("Merci d'entrer une première chaine de charactère : ");
+let str1 = scanf("%s");
+console.log("Merci d'entrer une seconde chaine de charactère : ");
+let str2 = scanf("%s");
+
+//algo
+if (str1.length >= 2*str2.length){
+    console.log("la première chaine de charactère est\n au moins deux fois plus longue que la seconde");
+}
+else{
+    console.log("la première chaine de charactère n'est\n pas deux fois plus longue que la seconde");
+}
